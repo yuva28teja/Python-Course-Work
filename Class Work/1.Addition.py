0a76fed980eb4955b420d1d3cd24fbc991d0ca98
yuva28teja <yuva2003teja@gmail.com>
2025-11-20 20:49:00 +0530

    Yuvateja

---
+++ Class Work/1.Addition.py
@@ -0,0 +1,7 @@
+a=24
+b=31
+print(a+b)
+
+#output: 55
+# This code adds two numbers, a and b, and prints the result.
+# The variables a and b are assigned the values 24 and 31, respectively.    
---
+++ Class Work/2.Keywords.py
@@ -0,0 +1,5 @@
+import keyword as kw 
+# This code imports the keywords module and prints the list of Python keywords.
+# The keywords module provides a list of reserved words in Python that cannot be used as identifiers.   
+print(kw.kwlist)
+# Output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
