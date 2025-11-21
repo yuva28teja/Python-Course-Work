ab8f46a22635ab6ac4fb1e3c44820088faebca48
Yuvateja Jagati <yuva2003teja@gmail.com>
2025-11-21 20:46:02 +0530

    Yuvateja

--- Class Work/03.Datatypes.py
+++ Class Work/03.Datatypes.py
@@ -7,73 +7,74 @@
 
 #int:
 product_quantity = 3
-order_id=1049543
-type(product_quantity)
+order_id = 1049543
+type(product_quantity)      # <class 'int'>
 
 #float
 product_price = 739.99
 discount = 45.5
-type(discount)
+type(discount)              # <class 'float'>
 
 #complex
-z=5+2j
-type(z)
+z = 5 + 2j
+type(z)                     # <class 'complex'>
 
 '''
-2.sequence data types
-->str-String
-->list-List
-->tuple->Tuple
+2. Sequence data types
+->str - String
+->list - List
+->tuple - Tuple
 '''
 
 #str
-name="Dinesh"
-Address="Nizampet,Hyderabad"
-print(type(Address))
+name = "Yuvateja"
+Address = "Pragati nagar rd,Hyderabad"
+print(type(Address))        # <class 'str'>
 
 #list
-cart_items = ["Shoes","shirts","Tshirts","headphones"]
-top_categories = ["Mobiles","Fashion","Home"]
-print(type(top_categories))
+cart_items = ["Shoes", "shirts", "Tshirts", "headphones"]
+top_categories = ["Mobiles", "Fashion", "Home"]
+print(type(top_categories)) # <class 'list'>
 
 #tuple
-warehouse_location = (12.954323,564324)
-product_dimensions = (12.5,9.7,34)
-print(type(product_dimensions))
+warehouse_location = (12.954323, 564324)
+product_dimensions = (12.5, 9.7, 34)
+print(type(product_dimensions))  # <class 'tuple'>
 
 '''
-3.set data types
+3. Set data types
 ->set
 ->frozenset
 '''
 
 #set
-available_sizes={"M","L","XL","XXL"}
-popular_brands={"Nike","Adidas","Puma","Nike"}
-print(type(available_sizes))
+available_sizes = {"M", "L", "XL", "XXL"}
+popular_brands = {"Nike", "Adidas", "Puma", "Nike"}
+print(type(available_sizes))     # <class 'set'>
 
 #frozenset
-frozen_tags =frozenset(["Sale","Limited edition","Trending"])
-print(type(frozen_tags))
+frozen_tags = frozenset(["Sale", "Limited edition", "Trending"])
+print(type(frozen_tags))         # <class 'frozenset'>
 
+#Mapping type
 
-#Mappingtype
-
-#Dict-Dictionary
-
-product_details={"name":"wireless mouse",
-                 "brand":"logitech",
-                 "Price":899.99,
-                 "rating":4.5}
-print(type(product_details))
+#Dict - Dictionary
+product_details = {
+    "name": "wireless mouse",
+    "brand": "logitech",
+    "Price": 899.99,
+    "rating": 4.5
+}
+print(type(product_details))     # <class 'dict'>
 
 #boolean type
-is_logged_in=True
-is_payment_successful=False
-is_in_stock=True
-print(type(is_logged_in))
+is_logged_in = True
+is_payment_successful = False
+is_in_stock = True
+print(type(is_logged_in))        # <class 'bool'>
 
 #none type
-tracking_number=None
-delivery_partner=None
-print(type(tracking_number))
+tracking_number = None
+delivery_partner = None
+print(type(tracking_number))     # <class 'NoneType'>
+
